de001117cf290ed21b3deb9a050b077360df83e9
Nacho0150 <ignacioibanez01@gmail.com>
2024-10-30 00:56:22 -0300

    Agregado todo lo de comercio

--- src/main/java/ar/com/tubarberia/entidades/Comercio.java
+++ src/main/java/ar/com/tubarberia/entidades/Comercio.java
@@ -66,5 +66,4 @@ public class Comercio {
     private Double calificacionPromedio;
 
     private String politicasCancelacion;
-
 }
--- src/main/java/ar/com/tubarberia/repositorios/ComercioRepository.java
+++ src/main/java/ar/com/tubarberia/repositorios/ComercioRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ComercioRepositorio extends JpaRepository<Comercio, Integer> {
+public interface ComercioRepository extends JpaRepository<Comercio, Long> {
 }
